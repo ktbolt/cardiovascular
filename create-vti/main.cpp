41cf2c5fa93489cdd692865ea4a10d2ab80177aa
Dave Parker <davep@stanford.edu>
2019-02-21 10:21:48 -0800

    Add some scaling for small models.

--- create-vti/main.cpp
+++ create-vti/main.cpp
@@ -1,18 +1,20 @@
-#include <vtkXMLPolyDataReader.h>
-#include <vtkSmartPointer.h>
+
+#include <vtkActor.h>
+#include <vtkImageData.h>
+#include <vtkImageDataGeometryFilter.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 #include <vtkPolyData.h>
 #include <vtkPointData.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkActor.h>
+#include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
 #include <vtkXMLPolyDataWriter.h>
 #include <vtkXMLImageDataWriter.h>
 #include <vtkXMLImageDataReader.h>
-#include <vtkImageData.h>
-#include <vtkImageDataGeometryFilter.h>
-#include <vtkProperty.h>
+#include <vtkXMLPolyDataReader.h>
 
 int main ( int argc, char *argv[] )
 {
@@ -40,35 +42,61 @@ int main ( int argc, char *argv[] )
   double ymax = bounds[3]; double zmin = bounds[4]; double zmax = bounds[5];
   double dx = xmax - xmin; double dy = ymax - ymin; double dz = zmax - zmin;
   double cx = xmin + dx / 2.0;    double cy = ymin + dy / 2.0;    double cz = zmin + dz / 2.0;
-  std::cout << "orig: cx " << cx << "  cy " << cy << "  cz " << cz << std::endl;
+  std::cout << "xmin: " << xmin << "  xmax: " << xmax << std::endl;
+  std::cout << "ymin: " << ymin << "  ymax: " << ymax << std::endl;
+  std::cout << "zmin: " << zmin << "  zmax: " << zmax << std::endl;
+  std::cout << "dx: " << dx << "  dy: " << dy << "  dz: " << dz << std::endl;
+  std::cout << "orig  cx: " << cx << "  cy: " << cy << "  cz: " << cz << std::endl;
 
-  // Translate surface center to (0,0,0).
+  double scale = 1.0;
+
+  if ((dx < 1.0) || (dy < 1.0) || (dz < 1.0)) {
+    scale = 10.0;
+  }
+  /*
+  dx *= scale;
+  dy *= scale;
+  dz *= scale;
+  */
+  std::cout << "scale: " << scale << std::endl;
+
+  // Translate surface.
   //
-  cx -= dx/2.0;
-  cy -= dy/2.0;
-  cz -= dz/2.0;
+  double tx, ty, tz;
+  tx = -xmin;
+  ty = -ymin;
+  tz = -zmin;
+
   vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
   points->ShallowCopy(polyData->GetPoints());
 
   for(vtkIdType i = 0; i < polyData->GetNumberOfPoints(); i++) {
     double *p = points->GetPoint(i); 
-    p[0] -= cx;
-    p[1] -= cy;
-    p[2] -= cz;
+    p[0] = p[0] + tx;
+    p[1] = p[1] + ty;
+    p[2] = p[2] + tz;
     points->SetPoint(i,p);
   }
 
   points->Modified(); 
+  polyData->Modified();
   polyData->GetBounds(bounds);
+
   xmin = bounds[0]; xmax = bounds[1]; ymin = bounds[2];
   ymax = bounds[3]; zmin = bounds[4]; zmax = bounds[5];
   dx = xmax - xmin; dy = ymax - ymin; dz = zmax - zmin;
+  dx *= scale;
+  dy *= scale;
+  dz *= scale;
   cx = xmin + dx / 2.0;    cy = ymin + dy / 2.0;    cz = zmin + dz / 2.0;
   if (dx == 2.0) dx = 3.0;
   if (dy == 2.0) dy = 3.0;
   if (dz == 2.0) dz = 3.0;
+  std::cout << "trans xmin: " << xmin << "  xmax: " << xmax << std::endl;
+  std::cout << "trans ymin: " << ymin << "  ymax: " << ymax << std::endl;
+  std::cout << "trans zmin: " << zmin << "  zmax: " << zmax << std::endl;
   std::cout << "trans: cx " << cx << "  cy " << cy << "  cz " << cz << std::endl;
-  std::cout << "dx " << dx << "  dy " << dy << "  dz " << dz << std::endl;
+  std::cout << "trans: dx " << dx << "  dy " << dy << "  dz " << dz << std::endl;
 
   // Write translated surface.
   //
@@ -89,14 +117,16 @@ int main ( int argc, char *argv[] )
   writer->Write();
 
   // Create image data.
+  //
   vtkSmartPointer<vtkImageData> imageData = vtkSmartPointer<vtkImageData>::New();
   int idx = int(dx);
   int idy = int(dy);
   int idz = int(dz);
-  std::cout << "idx " << idx << "  idy " << idy << "  idz " << idz << std::endl;
+  std::cout << "image idx: " << idx << "  idy: " << idy << "  idz: " << idz << std::endl;
   imageData->SetDimensions(idx,idy,idz);
   imageData->AllocateScalars(VTK_DOUBLE, 1);
   int* dims = imageData->GetDimensions();
+  std::cout << "image dims[0]:" << dims[0] << "  dims[1]:" << dims[1] << "  dims[2]:" << dims[2] << std::endl;
 
   // Fill every entry of the image data with "2.0"
   for (int z = 0; z < dims[2]; z++) {
@@ -116,7 +146,6 @@ int main ( int argc, char *argv[] )
 
   // Convert the image to a polydata
   //
-  //
   vtkSmartPointer<vtkXMLImageDataReader> ireader = vtkSmartPointer<vtkXMLImageDataReader>::New();
   ireader->SetFileName(image_file_name.c_str());
   ireader->Update();
@@ -126,24 +155,33 @@ int main ( int argc, char *argv[] )
   imageDataGeometryFilter->Update();
   vtkSmartPointer<vtkPolyDataMapper> imapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   imapper->SetInputConnection(imageDataGeometryFilter->GetOutputPort());
+  imapper->ScalarVisibilityOff();
   vtkSmartPointer<vtkActor> iactor = vtkSmartPointer<vtkActor>::New();
   iactor->SetMapper(imapper);
   iactor->GetProperty()->SetPointSize(1);
-  iactor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+  iactor->GetProperty()->SetColor(0.0, 0.0, 1.0);
 
-  // Visualize
+  // Create surface graphics geometry.
+  //
   vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
   mapper->SetInputConnection(reader->GetOutputPort());
-
+  mapper->ScalarVisibilityOff();
   vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
   actor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+  actor->GetProperty()->SetRepresentationToWireframe();
 
   vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
   vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
+  renderWindow->SetSize(1000,1000);
+
+  // Add trackball interactor.
+  //
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
+  vtkSmartPointer<vtkInteractorStyleTrackballCamera> style = vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New(); 
+  renderWindowInteractor->SetInteractorStyle(style);
 
   renderer->AddActor(actor);
   renderer->AddActor(iactor);
